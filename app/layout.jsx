a7d19c80f3cc9fcd381b618c03a3c4dd38bf61d2
matthew-simpson-test <matthew.simpson@puzzel.com>
2024-10-09 11:05:45 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,14 @@
-// app/layout.jsx
+import '../styles/globals.css';
+import { Footer } from '../components/footer';
+import { Header } from '../components/header';
 
-"use client";  // This must be at the very top of the file
-
-// Define metadata outside the Client Component
 export const metadata = {
     title: {
         template: '%s | Netlify',
-        default: 'Netlify Starter',
-    },
+        default: 'Netlify Starter'
+    }
 };
 
-import '../styles/globals.css';  // Import global styles
-import { Footer } from '../components/footer';  // Import your Footer component
-import { Header } from '../components/header';  // Import your Header component
-import Script from 'next/script';  // Import the Script component
-
-// RootLayout component
 export default function RootLayout({ children }) {
     return (
         <html lang="en" data-theme="lofi">
@@ -30,25 +23,6 @@ export default function RootLayout({ children }) {
                         <Footer />
                     </div>
                 </div>
-
-                {/* Add the chat widget script here */}
-                <Script
-                    id="puzzle-loader"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `(function (a, b) {
-                            var loader = a.createElement('script');
-                            loader.type = 'text/javascript';
-                            loader.src = 'https://app-cdn.puzzel.com/public/js/pzl_loader.js';
-                            loader.setAttribute('id', 'pzlModuleLoader');
-                            loader.setAttribute('data-customer-id', b);
-                            a.body.append(loader);
-                        })(document, '90125');`,
-                    }}
-                    onError={(e) => {
-                        console.error('Error loading Puzzel script:', e);
-                    }}
-                />
             </body>
         </html>
     );
